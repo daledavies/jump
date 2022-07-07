de09a06c489561e729beda142598566afe4baf58
Dale Davies <dale@daledavies.co.uk>
2022-07-07 16:14:51 +0100

    Add splash and version to entrypoint log output

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-if [ -z "${DEVELOPMENT-}" ]; then
-    echo >&2 "-------------------------------------------------------------"
 
+echo >&2 "-------------------------------------------------------------"
+echo >&2 "";
+echo >&2 "     ██ ██    ██ ███    ███ ██████"
+echo >&2 "     ██ ██    ██ ████  ████ ██   ██"
+echo >&2 "     ██ ██    ██ ██ ████ ██ ██████"
+echo >&2 "██   ██ ██    ██ ██  ██  ██ ██"
+echo >&2 " █████   ██████  ██      ██ ██"
+echo >&2 "";
+echo >&2 "https://github.com/daledavies/jump"
+echo >&2 "";
+echo >&2 "-------------------------------------------------------------"
+
+if [ -z "${DEVELOPMENT-}" ]; then
+    echo >&2 "";
     echo >&2 "- Repopulating web root with application files."
+
     if [ "$(ls -A /var/www/html)" ]; then
         rm /var/www/html/* -r
     fi
-    cp /usr/src/jumpapp/* /var/www/html -r
+    cp /usr/src/jumpapp/. /var/www/html -r
 
-    echo >&2 "- Check if backgrounds, search or sites volumes have been mounted."
+    echo >&2 "- You are using Jump $(</var/www/html/.jump-version)";
+    echo >&2 "";
+    echo >&2 "-------------------------------------------------------------"
+    echo >&2 "";
+
+    echo >&2 "- Checking if backgrounds, search or sites volumes have been mounted."
     if [ -e "/backgrounds" ]; then
         echo >&2 "   - Backgrounds directory is mapped... symlinking."
         rm /var/www/html/assets/backgrounds -r
@@ -41,8 +59,11 @@ if [ -z "${DEVELOPMENT-}" ]; then
         fi
     fi
 
+    echo >&2 "";
     echo >&2 "- All done! Starting nginx/php services now."
+    echo >&2 "";
     echo >&2 "-------------------------------------------------------------"
+    echo >&2 "";
 fi
 
 php-fpm8
