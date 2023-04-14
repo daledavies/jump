2842b7aded98379089988579268a5ef790b9b949
Michiel Dethmers <michiel@phplist.com>
2023-04-14 15:05:40 +0100

    Create nl.js
    Add Dutch translation

---
+++ jumpapp/translations/nl.js
@@ -0,0 +1,18 @@
+{
+    "greetings.goodafternoon": "Goedemiddag",
+    "greetings.goodevening": "Goedenavond",
+    "greetings.goodmorning": "Goedemorgen",
+    "greetings.goodnight": "Goedenavond",
+    "search.search": "Zoeken",
+    "search.searchon": "<span>Zoek via</span> {{searchprovider}}",
+    "search.sites": "Websites",
+    "status.status": "Status",
+    "status.error": "fout" ,
+    "status.offline": "offline",
+    "status.online": "online",
+    "status.unknown": "onbekend",
+    "tags": "Tags",
+    "tags.home": "home",
+    "unsplash.description.photoby": "Foto door {{user}}",
+    "unsplash.description.by": "{{description}} door {{user}}"
+}
